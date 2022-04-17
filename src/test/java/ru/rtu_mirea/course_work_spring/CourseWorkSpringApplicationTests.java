48c073dc49f77adacffe35f1f9fe4cd3e9877044
Mikhail OKhapkin <mikhail.okhapkin@yandex.ru>
2022-04-17 15:11:01 +0300

    First point

---
+++ src/test/java/ru/rtu_mirea/course_work_spring/CourseWorkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rtu_mirea.course_work_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
